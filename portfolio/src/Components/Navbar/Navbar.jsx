e55f8861f4a84c94300a637cd8a0e173b1e9c4cd
dayojaiye74 <dayojaiye74@gmail.com>
2022-08-12 08:50:55 +0100

    portfolio app

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
                     <li>Testimonials</li>
                 </ul>
             </div>
-            <button className="button">
+            <button className="button n-button">
                 Contact us
             </button>
         </div>
