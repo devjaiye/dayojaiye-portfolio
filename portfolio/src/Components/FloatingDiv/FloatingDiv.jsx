4f5b688a4d7d69125dd22517f7be89576f49ba56
dayojaiye74 <dayojaiye74@gmail.com>
2022-08-14 23:59:10 +0100

    portfolio app

--- portfolio/src/Components/FloatingDiv/FloatingDiv.jsx
+++ portfolio/src/Components/FloatingDiv/FloatingDiv.jsx
@@ -4,7 +4,8 @@ function FloatingDiv({image, txt1, txt2}) {
   return (
     <div className="floatingdiv">
         <img src={image} alt="" />
-        <span>{txt1}
+        {/* using props */}
+        <span>{txt1} 
             <br/>
             {txt2}
         </span>
