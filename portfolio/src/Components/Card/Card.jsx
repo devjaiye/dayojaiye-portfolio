98ed7f790400ffab647d3bd84e4eceb9f8fd2ca9
dayojaiye74 <dayojaiye74@gmail.com>
2022-08-19 00:57:09 +0100

    portfolio app

--- portfolio/src/Components/Card/Card.jsx
+++ portfolio/src/Components/Card/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Card.css'
 
-const Card = (emoji, heading, detail) => {
+const Card = ({emoji, heading, detail}) => {
   return (
    <div className="card">
-    
+        <img src={emoji} alt="" />
+        <span>{heading}</span>
+        <span>{detail}</span>
+
    </div>
   )
 }
