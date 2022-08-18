554e3bbdcefc2543b922186c85904ee835cd7c74
dayojaiye74 <dayojaiye74@gmail.com>
2022-08-18 23:22:38 +0100

    portfolio app

--- portfolio/src/Components/Services/Services.jsx
+++ portfolio/src/Components/Services/Services.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Services.css'
+import HeartEmoji from "../../img/heartemoji.png"
+import Glasses from "../../img/glasses.png"
+import Humble from "../../img/humble.png"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="services">
+        {/* left side */}
+        <div className="awesome">
+
+        </div>
+        {/* right side */}
+        <div className="cards">
+
+        </div>
+    </div>
   )
 }
 
