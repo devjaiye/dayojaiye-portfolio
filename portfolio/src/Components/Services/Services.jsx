c4ae5c5a65ee7923e303e0bfbda4f9b722238806
GlobalMadeDAO <dayo@mademarketingagency.com>
2022-08-21 22:28:07 +0100

    portfolio app

--- portfolio/src/Components/Services/Services.jsx
+++ portfolio/src/Components/Services/Services.jsx
@@ -4,7 +4,7 @@ import HeartEmoji from "../../img/heartemoji.png"
 import Glasses from "../../img/glasses.png"
 import Humble from "../../img/humble.png"
 import Card from '../Card/Card'
-import Resume from '../../resume.pdf'
+import Resume from './resume.pdf'
 
 const Services = () => {
   return (
