4f61382735f6b63e0903d0f598d6e3a12bc939ab
devjaiye <dayojaiye74@gmail.com>
2022-09-11 21:41:17 +0100

    portfolio app

--- portfolio/src/Components/Portfolio/Portfolio.jsx
+++ portfolio/src/Components/Portfolio/Portfolio.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Portfolio.css'
 import {Swiper, SwiperSlide} from 'swiper/react'
+import Sidebar from "../../img/sidebar.png"
+import Ecommerce from "../../img/ecommerce.png"
+import HOC from "../../img/hoc.png"
+import MusicApp from "../../img/musicapp.png"
 
 const Portfolio = () => {
   return (
@@ -9,7 +13,11 @@ const Portfolio = () => {
       <span>Recent Projects</span>
       <span>Portfolio</span>
       {/* slider */}
-      
+      <Swiper>
+        <SwiperSlide>
+          <img src="" alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
