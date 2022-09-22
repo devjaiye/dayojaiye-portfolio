eb3fdff303d6d77266bc773a8a32ccf977c54e5d
devjaiye <dayojaiye74@gmail.com>
2022-09-22 23:43:20 +0100

    portfolio app

--- portfolio/src/Components/Testimonials/Testimonials.jsx
+++ portfolio/src/Components/Testimonials/Testimonials.jsx
@@ -39,9 +39,9 @@ const Testimonial = () => {
   return (
     <div className='t-wrapper' id='Testimonials'>
       <div className="t-heading">
-        <span>Clients always get </span>
-        <span>Exceptional Service </span>
-        <span>from me</span>
+        <span>Clients </span>
+        <span>Feedback </span>
+        <span>From My Services</span>
         <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
       <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
       </div>
